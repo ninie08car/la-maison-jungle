2ef4083008286f94fbc09248eb72a9d5fe303aa3
Annie Berjou <annie.carrau@wanadoo.fr>
2026-02-17 18:50:58 +0100

    Création Shoppinglist

---
+++ src/components/ShoppingList.jsx
@@ -0,0 +1,19 @@
+const plantList = [
+  "monstera",
+  "ficus lyrata",
+  "pothos argenté",
+  "yucca",
+  "palmier",
+];
+
+const ShoppingList = () => {
+  return (
+    <ul>
+      {plantList.map((plant, index) => (
+        <li key={`${plant}-${index}`}></li>
+      ))}
+    </ul>
+  );
+};
+
+export default ShoppingList;
