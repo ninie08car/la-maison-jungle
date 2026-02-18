adb70d93ed8fbcdee457b9f18576bbc927cf99da
Annie Berjou <annie.carrau@wanadoo.fr>
2026-02-18 17:57:32 +0100

    Photo et fonction Vider le panier

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,20 +1,24 @@
 import "../styles/Cart.css";
+import { useState } from "react";
 
 const Cart = () => {
   const monsteraPrice = 8;
-  const ivyPrice = 10;
-  const flowerPrice = 15;
+  const [cart, updateCart] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-  return (
+  return isOpen ? (
     <div className="lmj-cart">
+      <button onClick={() => setIsOpen(false)}>Fermer</button>
       <h2>Panier</h2>
-      <ul>
-        <li>Monstera : {monsteraPrice}€</li>
-        <li>Lierre : {ivyPrice}€</li>
-        <li>Fleurs : {flowerPrice}€</li>
-      </ul>
-      Total : {monsteraPrice + ivyPrice + flowerPrice}€
+      <div>
+        Monstera : {monsteraPrice}€
+        <button onClick={() => updateCart(cart + 1)}> Ajouter </button>
+      </div>
+      <h3>Total : {monsteraPrice * cart}€</h3>
+      <button onClick={() => updateCart(0)}>Vider la panier</button>
     </div>
+  ) : (
+    <button onClick={() => setIsOpen(true)}>Ouvrir le panier</button>
   );
 };
 
