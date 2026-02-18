f6467b786d7f55081287ab5939a25d1bc8d38bf5
Annie Berjou <annie.carrau@wanadoo.fr>
2026-02-18 17:05:12 +0100

    formulaire footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,28 @@ import "../styles/Footer.css";
 const Footer = () => {
   const [inputValue, setInputValue] = useState("");
 
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleBlur = () => {
+    if (!inputValue.includes("@")) {
+      alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide");
+    }
+  };
+
   return (
     <footer className="lmj-footer">
       <div className="lmj-footer-elem">
         Pour les passionné·e·s de plantes 🌿🌱🌵
       </div>
       <div className="lmj-footer-elem">Laissez-nous votre mail :</div>
-      <textarea
+      <input
+        placeholder="Entrez votre mail"
+        onChange={handleInput}
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onBlur={handleBlur}
       />
-      <button onClick={() => alert(inputValue)}>Envoyer</button>
     </footer>
   );
 };
